4a70f2b0093f8be6e57075cbc521241ed6b5d69d
wajima <goo.hiro.w@gmail.com>
2023-03-28 15:17:51 +0900

    type

--- scripts/01_basic.js
+++ scripts/01_basic.js
@@ -2,9 +2,12 @@ var Basic;
 (function (Basic) {
     var hello = "Hello TypeScript";
     console.log(hello);
+    // ----- 型 -----
+    function add(n1, n2) {
+        return n1 + n2;
+    }
+    var number1 = 3;
+    var number2 = 4.5;
+    var result = add(number1, number2);
+    console.log(result);
 })(Basic || (Basic = {}));
-var Basic2;
-(function (Basic2) {
-    var hello = "Hello TypeScript2!";
-    console.log(hello);
-})(Basic2 || (Basic2 = {}));
--- scripts/01_basic.ts
+++ scripts/01_basic.ts
@@ -1,9 +1,14 @@
 namespace Basic {
   const hello = "Hello TypeScript";
   console.log(hello);
-}
 
-namespace Basic2 {
-  const hello = "Hello TypeScript2!";
-  console.log(hello);
+  // ----- 型 -----
+  function add(n1: number, n2: number) {
+    return n1 + n2;
+  }
+
+  const number1 = 3;
+  const number2 = 4.5;
+  const result = add(number1, number2);
+  console.log(result);
 }
