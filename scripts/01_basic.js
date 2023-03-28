80ffa176c0902245ba0b90e2ed549407d040849f
wajima <goo.hiro.w@gmail.com>
2023-03-28 16:04:20 +0900

    number, string, boolean

--- scripts/01_basic.js
+++ scripts/01_basic.js
@@ -3,15 +3,22 @@ var Basic;
     var hello = "Hello TypeScript";
     console.log(hello);
     // ----- 型 -----
-    function add(n1, n2) {
+    function add(n1, n2, showResult, phrase) {
         // JavaScriptの型チェック
         // if(typeof n1 !== "number" || typeof n2 !== "number") {
         //   throw new Error("入力値が不正です");
         // }
-        return n1 + n2;
+        var result = n1 + n2;
+        if (showResult) {
+            console.log(phrase + result);
+        }
+        else {
+            return result;
+        }
     }
     var number1 = 3;
     var number2 = 4.5;
-    var result = add(number1, number2);
-    console.log(result);
+    var printResult = true;
+    var resultPhrase = "Result: ";
+    add(number1, number2, printResult, resultPhrase);
 })(Basic || (Basic = {}));
--- scripts/01_basic.ts
+++ scripts/01_basic.ts
@@ -3,16 +3,23 @@ namespace Basic {
   console.log(hello);
 
   // ----- 型 -----
-  function add(n1: number, n2: number) {
+  function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     // JavaScriptの型チェック
     // if(typeof n1 !== "number" || typeof n2 !== "number") {
     //   throw new Error("入力値が不正です");
     // }
-    return n1 + n2;
+    const result = n1 + n2;
+    if(showResult) {
+      console.log(phrase + result);
+    } else {
+      return result;
+    }
   }
 
   const number1 = 3;
   const number2 = 4.5;
-  const result = add(number1, number2);
-  console.log(result);
+  const printResult = true;
+  const resultPhrase = "Result: ";
+
+  add(number1, number2, printResult, resultPhrase);
 }
