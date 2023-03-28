e69cc90f8d8f1a6806c5b3473903157b0bd745bb
wajima <goo.hiro.w@gmail.com>
2023-03-28 15:03:25 +0900

    namespace

--- scripts/01_basic.js
+++ scripts/01_basic.js
@@ -1,1 +1,10 @@
-console.log("Hello TypeScript");
+var Basic;
+(function (Basic) {
+    var hello = "Hello TypeScript";
+    console.log(hello);
+})(Basic || (Basic = {}));
+var Basic2;
+(function (Basic2) {
+    var hello = "Hello TypeScript2!";
+    console.log(hello);
+})(Basic2 || (Basic2 = {}));
--- scripts/01_basic.ts
+++ scripts/01_basic.ts
@@ -1,1 +1,9 @@
-console.log("Hello TypeScript");
+namespace Basic {
+  const hello = "Hello TypeScript";
+  console.log(hello);
+}
+
+namespace Basic2 {
+  const hello = "Hello TypeScript2!";
+  console.log(hello);
+}
