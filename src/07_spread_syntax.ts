7e339e08bd6f10988f0620d029fb7ab6717b3eb8
wajima <goo.hiro.w@gmail.com>
2023-04-14 13:28:57 +0900

    spread syntax

---
+++ src/07_spread_syntax.ts
@@ -0,0 +1,21 @@
+namespace Sandbox07 {
+  console.log("----- 07 -----");
+
+  const hobbies = ['Sports', 'Cooking'];
+  const activeHobbies = ['Socker'];
+
+  // activeHobbies.push(hobbies[0], hobbies[1]);
+  activeHobbies.push(...hobbies);
+  console.log(activeHobbies);
+
+  const person = {
+    name: 'Obama', 
+    age: 61, 
+  };
+
+  const copiedPerson = {
+    ...person,
+  };
+
+  console.log(copiedPerson);
+}
