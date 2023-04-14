7445df84fd8c84d0f8e196f5d44316c5f877da0e
wajima <goo.hiro.w@gmail.com>
2023-04-14 15:23:35 +0900

    this

--- src/10_class.ts
+++ src/10_class.ts
@@ -7,8 +7,17 @@ namespace Sandbox10 {
     constructor(n: string) {
       this.name = n;
     }
+
+    describe(this: Department) {
+      console.log("Department: " + this.name);
+    }
   }
 
   const accounting = new Department('Accounting');
   console.log(accounting);
+  accounting.describe();
+
+  const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+  accountingCopy.describe();
+
 }
