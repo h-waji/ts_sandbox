9831a2a9a71a775611be9490ba7a492eea8def3a
wajima <goo.hiro.w@gmail.com>
2023-04-14 14:47:00 +0900

    class

---
+++ src/10_class.ts
@@ -0,0 +1,14 @@
+namespace Sandbox10 {
+  console.log("----- 10 -----");
+
+  class Department {
+    name: string;
+
+    constructor(n: string) {
+      this.name = n;
+    }
+  }
+
+  const accounting = new Department('Accounting');
+  console.log(accounting);
+}
