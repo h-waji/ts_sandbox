90c2e8ba0ca4ebfbdc8314ee498bd1de2fd89a9b
wajima <goo.hiro.w@gmail.com>
2023-04-15 17:03:23 +0900

    type guards

--- src/12_intersection_types.ts
+++ src/12_intersection_types.ts
@@ -24,4 +24,59 @@ namespace Sandbox12 {
   type Numeric = number | boolean;
 
   type Universal = Combinable & Numeric;
+
+  function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+      return a.toString() + b.toString();
+    }
+    return a + b;
+  }
+
+  type UnknownEmployee = Employee | Admin;
+
+  function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log(emp.name);
+    if ('privileges' in emp) {
+      console.log("Privileges:", emp.privileges);
+    }
+    if ('startDate' in emp) {
+      console.log("StartDate:", emp.startDate);
+    }
+  }
+
+  printEmployeeInformation(e1);
+
+  class Car {
+    drive() {
+      console.log('運転中');
+    }
+  }
+
+  class Truck {
+    drive() {
+      console.log('トラックを運転中');
+    }
+
+    loadCargo(amount: number) {
+      console.log('荷物を載せています', amount);
+    }
+  }
+
+  type Vehicle = Car | Truck;
+
+  const v1 = new Car();
+  const v2 = new Truck();
+
+  function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    // if ('loadCargo' in vehicle) {
+    //   vehicle.loadCargo(1000);
+    // }
+    if (vehicle instanceof Truck) {
+      vehicle.loadCargo(1000);
+    }
+  }
+
+  useVehicle(v1);
+  useVehicle(v2);
 }
