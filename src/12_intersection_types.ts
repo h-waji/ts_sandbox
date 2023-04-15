7db33383c9df7ad2dd5c346ee0d840bda197af4f
wajima <goo.hiro.w@gmail.com>
2023-04-15 17:40:26 +0900

    discriminated unions

--- src/12_intersection_types.ts
+++ src/12_intersection_types.ts
@@ -79,4 +79,31 @@ namespace Sandbox12 {
 
   useVehicle(v1);
   useVehicle(v2);
+
+  interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+  }
+
+  interface Horse {
+    type: 'horse';
+    runningSpeed: number;
+  }
+
+  type Animal = Bird | Horse;
+
+  function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+      case 'bird':
+        speed = animal.flyingSpeed;
+        break;
+      case 'horse':
+        speed = animal.runningSpeed;
+        break;
+    }
+    console.log('移動速度:', speed);
+  }
+
+  moveAnimal({ type: 'bird', flyingSpeed: 10 });
 }
