ee8643b3d1daba1b047ffe8252584433c5621ae7
wajima <goo.hiro.w@gmail.com>
2023-04-15 13:51:06 +0900

    interface, implements

--- src/11_interface.ts
+++ src/11_interface.ts
@@ -1,22 +1,36 @@
 namespace Sandbox11 {
   console.log("----- 11 -----");
 
-  interface Person {
+  interface Greetable {
     name: string;
-    age: number;
 
     greet(phrase: string): void;
   }
 
-  let user1: Person;
+  class Person implements Greetable {
+    name: string;
+    age: number;
 
-  user1 = {
-    name: 'Obama', 
-    age: 61, 
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+
     greet(phrase: string) {
       console.log(phrase, this.name);
     }
   }
+
+  let user1: Greetable;
+  user1 = new Person('Obama', 61);
+
+  // user1 = {
+  //   name: 'Obama', 
+  //   age: 61, 
+  //   greet(phrase: string) {
+  //     console.log(phrase, this.name);
+  //   }
+  // }
 
   user1.greet('Hello my name is');
 
