eb32a58c688f79a8a6824fab0fbb1525e6df4f7b
wajima <goo.hiro.w@gmail.com>
2023-04-15 19:26:12 +0900

    optional chaining

---
+++ src/14_optional_chaining.ts
@@ -0,0 +1,16 @@
+namespace Sandbox14 {
+  console.log("----- 14 -----");
+
+  const fetchedUserData = {
+    id: 'u1', 
+    name: 'user1',
+    job: {
+      title: 'Developer', 
+      description: 'TypeScript',
+    }
+  }
+
+  // console.log(fetchedUserData.job && fetchedUserData.job.title);
+  console.log(fetchedUserData?.job?.title);
+
+}
