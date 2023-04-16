648875f1927f7555ad7c895c421c7ed3d900a28a
wajima <goo.hiro.w@gmail.com>
2023-04-16 14:13:48 +0900

    generics, extends

--- src/17_generic_function.ts
+++ src/17_generic_function.ts
@@ -4,7 +4,7 @@ namespace Sandbox17 {
   // function merge(objA: object, objB: object) {
   //   return Object.assign(objA, objB);
   // }
-  function merge<T, U>(objA: T, objB: U) {
+  function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
   }
 
