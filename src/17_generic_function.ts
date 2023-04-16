45894993ad7af2e8a2e537801c2cb9c9d2e4f157
wajima <goo.hiro.w@gmail.com>
2023-04-16 14:32:36 +0900

    generics, interface

--- src/17_generic_function.ts
+++ src/17_generic_function.ts
@@ -10,4 +10,19 @@ namespace Sandbox17 {
 
   const mergedObj = merge({ name: 'Obama' }, { age: 61 });
   console.log(mergedObj.age);
+
+
+  interface Lengty {
+    length: number;
+  }
+
+  function countAndDescribe<T extends Lengty>(element: T): [T, string] {
+    let discriptionText = '値がありません。';
+    if (element.length > 0) {
+      discriptionText = `値は${element.length}個です。`;
+    }
+    return [element, discriptionText];
+  }
+
+  console.log(countAndDescribe('今日は雨が降っています。'));
 }
