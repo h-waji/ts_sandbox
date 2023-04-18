483b2d022f1794e89f939b6616f383ff727c92f0
wajima <goo.hiro.w@gmail.com>
2023-04-18 20:02:22 +0900

    accessor, method, parameter decorator

--- src/20_property_decorator.ts
+++ src/20_property_decorator.ts
@@ -6,11 +6,33 @@ namespace Sandbox20 {
     console.log(target, propertyName);
   }
 
+  function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log("Accessor decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+  }
+
+  function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log("Method decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+  }
+
+  function Log4(target: any, name: string | Symbol, position: number) {
+    console.log("Parameter decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+  }
+
   class Product {
     @Log
     title: string;
     private _price: number;
 
+    @Log2
     set price(val: number) {
       if (val > 0) {
         this._price = val;
@@ -24,7 +46,8 @@ namespace Sandbox20 {
       this._price = p;
     }
 
-    getPriceWithTax(tax: number) {
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
       return this._price * (1 + tax);
     }
   }
