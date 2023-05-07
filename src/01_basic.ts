e5e453a661312c9f8b3eb092a9e052589a6d7988
wajima <goo.hiro.w@gmail.com>
2023-05-07 23:34:02 +0900

    basic

--- src/01_basic.ts
+++ src/01_basic.ts
@@ -1,27 +1,21 @@
-// namespace Sandbox01 {
-//   console.log("----- 01 -----");
+const hello = 'Restart TypeScript';
+console.log(hello);
 
-//   const hello = "Hello TypeScript";
-//   console.log(hello);
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+    throw new Error('入力値が不正です');
+  }
+  const result = n1 + n2;
+  if(showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
 
-//   // ----- 型 -----
-//   function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-//     // JavaScriptの型チェック
-//     // if(typeof n1 !== "number" || typeof n2 !== "number") {
-//     //   throw new Error("入力値が不正です");
-//     // }
-//     const result = n1 + n2;
-//     if(showResult) {
-//       console.log(phrase + result);
-//     } else {
-//       return result;
-//     }
-//   }
-
-//   const number1 = 3;
-//   const number2 = 4.5;
-//   const printResult = true;
-//   const resultPhrase = "Result: ";
-
-//   add(number1, number2, printResult, resultPhrase);
-// }
+// console.log(add(5, 7));
+const number1 = 5;
+const number2 = 7;
+const printResult = true;
+const phrase = '合計：';
+add(number1, number2, printResult, phrase);
