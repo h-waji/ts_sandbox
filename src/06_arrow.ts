a606147dee53543390c96358f3e4545b966291df
wajima <goo.hiro.w@gmail.com>
2023-06-10 22:37:25 +0900

    arrow

--- src/06_arrow.ts
+++ src/06_arrow.ts
@@ -1,27 +1,33 @@
-// namespace Sandbox06 {
-//   console.log("----- 06 -----");
+namespace Sandbox06 {
+  console.log("----- 06 -----");
 
-//   // const add = (a: number, b: number) => {
-//   //   return a + b;
-//   // };
-//   // デフォルト値は一番右か全部に
-//   const add = (a: number, b: number = 1) => a + b;
+  // const add = (a:number, b: number) => {
+  //   return a + b;
+  // };
+  // console.log(add(1, 3));
 
-//   // const printOutput = (output: string | number) => {
-//   //   console.log(output);
-//   // }
-//   // constの型を関数型として指定
-//   const printOutput: (output: string | number) => void = output => {
-//     console.log(output);
-//   }
+  // デフォルト値は一番右か全部に
+  const add = (a: number, b: number = 1) => a + b;
+  console.log(add(5));
 
-//   console.log(add(4));
-//   printOutput(add(4, 14));
+  // const printOutput = (output: string | number) => {
+  //   console.log(output);
+  // }
+  // printOutput(5);
+  // printOutput("５");
 
-//   const button = document.querySelector('button');
-//   if (button) {
-//     button.addEventListener('click', event => {
-//       console.log(event);
-//     });
-//   }
-// }
+  // constの型を関数型として指定
+  //   (output: string | number) => void
+  const printOutput: (output: string | number) => void = output => {
+    console.log(output);
+  }
+  printOutput(55);
+  printOutput("５５");
+
+  const button = document.querySelector('button');
+  if(button) {
+    button.addEventListener('click', event => {
+      console.log(event);
+    })
+  }
+}
